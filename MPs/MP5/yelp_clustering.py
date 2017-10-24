b6daa0d9ad6ade6ab404a45db9d469e01a0470ce
tiopi <oac2@illinois.edu>
2017-10-23 23:50:25 -0500

    Update yelp_clustering.py

--- MPs/MP5/yelp_clustering.py
+++ MPs/MP5/yelp_clustering.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("Yelp Clustering")
 sc = SparkContext(conf=conf)
 
-businesses = sc.textFile("hdfs://dataproc-3ba9e17b-802e-4fec-8f2d-4e0d4167cadb-us-central1/Datasets/yelp/business.json")
+businesses = sc.textFile("gs://dataproc-3ba9e17b-802e-4fec-8f2d-4e0d4167cadb-us-central1/Datasets/yelp/business.json")
 
 with open('yelp_clustering.txt', 'w+') as f:
     pass
